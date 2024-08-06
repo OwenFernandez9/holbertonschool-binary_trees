1f0fda57a95ec33adc8233710b51c49b277ac06b
OwenFernandez9 <9357@holbertonstudents.com>
2024-08-06 18:39:14 +0000

    pre-order transversal

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+/**
+ * print_num - Prints a number
+ *
+ * @n: Number to be printed
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+}
